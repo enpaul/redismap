9ddaee63a290cb29e1c6836fd92c8a29a8e731f3
Ethan Paul <24588726+enpaul@users.noreply.github.com>
2021-02-24 16:16:53 -0500

    Initial commit

---
+++ redismap.py
@@ -0,0 +1,8 @@
+"""Wrapper for Redis servers to support using a server as a python mapping"""
+
+__title__ = "redismap"
+__summary__ = "A Redis wrapper that lets Redis be used as a python mapping"
+__version__ = "0.0.0"
+__url__ = "https://github.com/enpaul/redismap/"
+__license__ = "MIT"
+__authors__ = ["Ethan Paul <24588726+enpaul@users.noreply.github.com>"]
