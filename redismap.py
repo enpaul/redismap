71ee40554d15dcd3b7d77fd3c14cbe64f64fa2ad
Ethan Paul <24588726+enpaul@users.noreply.github.com>
2021-02-24 16:38:39 -0500

    Bump feature version

--- redismap.py
+++ redismap.py
@@ -16,7 +16,7 @@ import redis
 
 __title__ = "redismap"
 __summary__ = "A Redis wrapper that lets Redis be used as a python mapping"
-__version__ = "0.0.0"
+__version__ = "0.1.0"
 __url__ = "https://github.com/enpaul/redismap/"
 __license__ = "MIT"
 __authors__ = ["Ethan Paul <24588726+enpaul@users.noreply.github.com>"]
